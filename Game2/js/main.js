464103ef383b92d0bdd7744bcca6b51b676baa4f
nolan4509 <nolanm4509@gmail.com>
2017-02-14 17:37:36 -0500

    Game2 folder

---
+++ Game2/js/main.js
@@ -0,0 +1,60 @@
+window.onload = function() {
+    // You might want to start with a template that uses GameStates:
+    //     https://github.com/photonstorm/phaser/tree/v2.6.2/resources/Project%20Templates/Basic
+    
+    // You can copy-and-paste the code from any of the examples at http://examples.phaser.io here.
+    // You will need to change the fourth parameter to "new Phaser.Game()" from
+    // 'phaser-example' to 'game', which is the id of the HTML element where we
+    // want the game to go.
+    // The assets (and code) can be found at: https://github.com/photonstorm/phaser/tree/master/examples/assets
+    // You will need to change the paths you pass to "game.load.image()" or any other
+    // loading functions to reflect where you are putting the assets.
+    // All loading functions will typically all be found inside "preload()".
+    
+    "use strict";
+    
+    var game = new Phaser.Game( 900, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
+    
+    function preload() {
+        // Load an image and call it 'logo'.
+        game.load.image( 'logo', 'assets/phaser.png' );
+        game.load.image( 'cowboy', 'assets/Cowboy4_idle with gun_0.png');
+        game.load.image( 'asteroid', 'assets/asteroid.jpg');
+    }
+    
+    var player;
+    
+    function create() {
+        // Create a sprite at the center of the screen using the 'logo' image.
+        player = game.add.sprite( game.world.centerX, game.world.centerY, 'cowboy' );
+        // Anchor the sprite at its center, as opposed to its top-left corner.
+        // so it will be truly centered.
+        player.anchor.setTo( 0.5, 0.5 );
+        
+        // Turn on the arcade physics engine for this sprite.
+        game.physics.enable( player, Phaser.Physics.ARCADE );
+        // Make it bounce off of the world bounds.
+        player.body.collideWorldBounds = true;
+        
+
+
+
+
+
+
+        // Add some text using a CSS style.
+        // Center it in X, and position its top 15 pixels from the top of the world.
+        var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
+        var text = game.add.text( game.world.centerX, 15, "Dodge the Asteroids!", style );
+        text.anchor.setTo( 0.5, 0.0 );
+    }
+    
+    function update() {
+        // Accelerate the 'player' sprite towards the cursor,
+        // accelerating at 300 pixels/second and moving no faster than 400 pixels/second
+        // in X or Y.
+        // This function returns the rotation angle that makes it visually match its
+        // new trajectory.
+        player.rotation = game.physics.arcade.accelerateToPointer( player, this.game.input.activePointer, 300, 400, 400 );
+    }
+};
